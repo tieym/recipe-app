164fb5577da025ce1878595f54746ef9091c7f3d
tieym <timdjong@hotmail.com>
2023-08-08 01:29:04 +0200

    fixed db issue, settings.py contained wrong password

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,8 @@
 """
-Test custom Django managements commands.
+Test custom Django management commands.
 """
 from unittest.mock import patch
+
 from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
@@ -9,25 +10,24 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch("core.management.commands.wait_for_db.Command.check")
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    
+    """Test commands."""
+
     def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
 
-    """ 
-    patch the sleep method so the app doesn't actually wait when testing (its a mock db anyway)
-    """
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-
+        """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
